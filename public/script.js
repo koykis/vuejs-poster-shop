11dc887f61b6fb11ecfa53287f5fdae9b9d4bbda
Koykis <koykis@gmail.com>
2017-05-18 18:18:23 +0300

    Initial commit

--- public/script.js
+++ public/script.js
@@ -1,1 +1,24 @@
-console.log('It works.');
+//console.log(Vue);
+new Vue({
+	el: '#app',
+	data: {
+		total: 0,
+		items: [
+			{ title: 'Mpiftekia' },
+			{ title: 'Souvlakia' },
+			{ title: 'Mprizoles' }
+		],
+		cart: []
+	},
+	methods: {
+		addItem: function(index) {
+			this.total+=9.99;
+			var item = this.items[index];
+			this.cart.push({
+				title: item.title,
+				qty: 1
+			});
+			console.log(this.cart.length);
+		}
+	}
+});
