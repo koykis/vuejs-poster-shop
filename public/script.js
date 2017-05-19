264b91d79c527695eaf4fdbc90df48ecd7f5d309
Koykis <koykis@gmail.com>
2017-05-19 13:31:05 +0300

    Completed the functionality in the cart items. Also added a REMOVE button that was not in the course, because I do not like removing the items by zeroing the quentity.

--- public/script.js
+++ public/script.js
@@ -1,24 +1,62 @@
-//console.log(Vue);
+var PRICE = 4.99;
+
 new Vue({
 	el: '#app',
 	data: {
 		total: 0,
 		items: [
-			{ title: 'Mpiftekia' },
-			{ title: 'Souvlakia' },
-			{ title: 'Mprizoles' }
+			{ id: 1, title: 'Mpiftekia' },
+			{ id: 2, title: 'Souvlakia' },
+			{ id: 3, title: 'Mprizoles' }
 		],
 		cart: []
 	},
 	methods: {
 		addItem: function(index) {
-			this.total+=9.99;
+			this.total += PRICE;
 			var item = this.items[index];
-			this.cart.push({
-				title: item.title,
-				qty: 1
-			});
-			console.log(this.cart.length);
+			var found = false;
+			for(var i = 0; i < this.cart.length; i++){
+				if(this.cart[i].id === item.id){
+					found = true;
+					this.cart[i].qty++;
+				}
+			}
+			if(!found){
+				this.cart.push({
+					id: item.id,
+					title: item.title,
+					price: PRICE,
+					qty: 1
+				});
+			}
+			//console.log(this.cart.length);
+		},
+		inc: function(item) {
+			item.qty++;
+			this.total += PRICE;
+		},
+		dec: function(item) {
+			if(item.qty > 1){
+				item.qty--;
+				this.total -= PRICE;
+			}else{
+				alert('You only have 1!!');
+			}
+		},
+		rem: function (item) {
+			var sure = confirm('Are you sure you want to remove ' + item.title + ' from your cart?');
+			for(var i=0; i <= this.cart.length; i++){
+				if(this.cart[i].id === item.id && sure === true){
+					this.cart.splice(i, 1);
+					this.total = this.total - (PRICE * item.qty);
+				}
+			}
+		}
+	},
+	filters: {
+		currency: function(price) {
+			return price.toFixed(2)+'€';
 		}
 	}
 });
